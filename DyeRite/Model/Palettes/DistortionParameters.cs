20f209a1ca697453a68b37b179ecfba73e5882b2
Xcelled <j12@ketelaar.com>
2015-10-28 21:03:50 -0400

    Added some basic classes

---
+++ DyeRite/Model/Palettes/DistortionParameters.cs
@@ -0,0 +1,26 @@
+﻿namespace DyeRite.Model.Palettes
+{
+	public class DistortionParameters
+	{
+		public int Number { get; }
+
+		public int Type { get; }
+		public int DistortMapId { get; }
+		public double Rate { get; }
+
+		/// <summary>
+		/// Initializes a new instance of the <see cref="DistortionParameters"/> class.
+		/// </summary>
+		/// <param name="number">The number.</param>
+		/// <param name="type">The type.</param>
+		/// <param name="distortMapId">The distort map identifier.</param>
+		/// <param name="rate">The rate.</param>
+		public DistortionParameters(int number, int type, int distortMapId, double rate)
+		{
+			Number = number;
+			Type = type;
+			DistortMapId = distortMapId;
+			Rate = rate;
+		}
+	}
+}
---
+++ DyeRite/Model/Palettes/RawPalette.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+
+namespace DyeRite.Model.Palettes
+{
+	public class RawPalette
+	{
+		public string Name { get; }
+
+		public int Width { get; }
+		public int Height { get; }
+
+		public byte[] Data { get; }
+
+		public List<DistortionParameters> Distortions { get; } = new List<DistortionParameters>();
+
+		/// <summary>
+		/// Initializes a new instance of the <see cref="RawPalette" /> class.
+		/// </summary>
+		/// <param name="name">The name.</param>
+		/// <param name="width">The width.</param>
+		/// <param name="height">The height.</param>
+		/// <param name="data">The data.</param>
+		public RawPalette(string name, int width, int height, byte[] data)
+		{
+			Name = name;
+			Width = width;
+			Height = height;
+			Data = data;
+		}
+	}
+}
