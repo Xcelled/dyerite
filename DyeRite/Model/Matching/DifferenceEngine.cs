5907cc9fae7e44c5ee9f5e40f8ed122b21b8078e
Xcelled <j12@ketelaar.com>
2015-10-29 02:05:15 -0400

    Added DifferenceEngine

---
+++ DyeRite/Model/Matching/DifferenceEngine.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ColorMine.ColorSpaces;
+using ColorMine.ColorSpaces.Comparisons;
+
+namespace DyeRite.Model.Matching
+{
+	public class DifferenceEngine
+	{
+		private static readonly CieDe2000Comparison CieDe2000 = new CieDe2000Comparison();
+
+		public ColorMap Calculate(Lab target, Lab[,] palette)
+		{
+			var height = palette.GetLength(0);
+			var width = palette.GetLength(1);
+
+			var results = new double[height, width];
+
+			Parallel.For(0, height, i =>
+			{
+				for (var j = 0; j < width; j++)
+					results[i, j] = palette[i, j].Compare(target, CieDe2000);
+			});
+
+			return new ColorMap(width, height, results);
+		}
+	}
+}
